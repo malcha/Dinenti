999bd9d02b1b12ece11f47a3bc5a56e6b8236abf
malcha <malcha@gmail.com>
2018-08-19 08:54:38 -0300

    parte 4

---
+++ Controllers/TeensController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using MvcApplication2.Manager;
+using MvcApplication2.Models;
+using System.Linq;
+using MvcApplication2.Helpers;
+
+namespace MvcApplication2.Controllers
+{
+    public class TeensController: Controller
+    {
+        public ActionResult Ellas()
+        {
+            Cookies.WriteCookie();
+            ViewBag.TitleName = "Ellas";
+            var list = new ClouthManager()
+                            .FindAll()
+                            .Where(x => x.Category == Clouth.CATEGORY.TEENS)
+                            .Where(x => x.SubCategory == Clouth.SUBCATEGORY.NENA)
+                            .OrderBy(x => x.Id)
+                            .ToList();
+
+            return View("todos", list);
+        }
+
+        public ActionResult Ellos()
+        {
+            Cookies.WriteCookie();
+            ViewBag.TitleName = "Ellos";
+            var list = new ClouthManager()
+                            .FindAll()
+                            .Where(x => x.Category == Clouth.CATEGORY.TEENS)
+                            .Where(x => x.SubCategory == Clouth.SUBCATEGORY.NENE)
+                            .OrderBy(x => x.Id)
+                            .ToList();
+
+            return View("todos", list);
+        }
+
+    }
+}
